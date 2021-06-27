ae8136b92f2b5e45cd049ef84549c05ac1d97bc5
Ross Carran <carran.ross@gmail.com>
2021-06-28 11:15:17 +1200

    added base class

---
+++ Battletech-Mod-Save-Hookin/Battletech-Mod-Save-Hookin.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace BattletechModSaveHookin
+{
+    public class Battletech_Mod_Save_Hookin
+    {
+        public Battletech_Mod_Save_Hookin()
+        {
+        }
+    }
+}
