201d2255a612229f99503024dc0eb42ddd913313
Ross-Carran <carran.ross@gmail.com>
2021-10-22 11:59:21 +1300

    * Globals.cs: Cleaned up so SqlLightTest is only created once   instead of each time Dbrun is called.
    * UserPrefs.xml: This gets changed everytime I do something.

--- Battletech-Mod-Save-Hookin/Globals.cs
+++ Battletech-Mod-Save-Hookin/Globals.cs
@@ -3,9 +3,11 @@ namespace BattletechModSaveHookin
 {
     static class Globals
     {
+        // create a perminant instance of SqLightTest instead of re-creating it every time Dbrun() is called
+        private static SqLightTest test = new SqLightTest();
+
         public static void Dbrun()
         {
-            SqLightTest test = new SqLightTest();
             test.Start();
         }
     }
