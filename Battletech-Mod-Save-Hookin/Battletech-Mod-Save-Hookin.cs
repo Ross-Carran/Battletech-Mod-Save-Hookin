014e3905bf661dccb22b8ab2abf3b274853fd5cb
Ross-Carran <carran.ross@gmail.com>
2021-10-24 06:49:22 +1300

    Added in tentative hookin point for loading combat save data

--- Battletech-Mod-Save-Hookin/Battletech-Mod-Save-Hookin.cs
+++ Battletech-Mod-Save-Hookin/Battletech-Mod-Save-Hookin.cs
@@ -21,11 +21,27 @@ namespace BattletechModSaveHookin
         public class Sql_Hookin_Location_Patch
         {
             [HarmonyPostfix]
-            public static void Postfix(GameInstanceSave __instance)
+            public static void Postfix(GameInstanceSave __instance, GameInstance gameInstance)
             {
                 FileLog.Log("A Save has been made");
                 Globals.Dbrun();
-                FileLog.Log("hello world");
+                FileLog.Log("Save Time: " + __instance.SaveTime.Ticks.ToString());
+                FileLog.Log("FileGUID: " + __instance.InstanceGUID);
+                FileLog.Log("Commander Name: " + gameInstance.Simulation.Commander.Name);
+                FileLog.Log("Company Name: " + gameInstance.Simulation.CompanyName);
+            }
+        }
+
+        [HarmonyPatch(typeof(GameInstance))]
+        [HarmonyPatch("CreateCombatFromSave")]
+        public class Sql_Hookin_Combat_Load_Patch
+        {
+            [HarmonyPostfix]
+            public static void Postfix(GameInstanceSave save)
+            {
+                FileLog.Log("Combat save being loaded");
+                FileLog.Log("Save Time: " + save.SaveTime.Ticks.ToString());
+                FileLog.Log("FileGUID: " + save.InstanceGUID);
             }
         }
     }
